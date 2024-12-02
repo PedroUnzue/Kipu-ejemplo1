7cbd6b446cd6b9eb5fa11c5c6a1d98d8506cad61
“Pedro <pedroeunzue@gmail.com>
2024-12-03 00:30:16 +0100

    Archivo index.js modificado

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const {Web3} = require('web3')
+const web3 = new Web3("https://eth.llamarpc.com");
 
 app.get('/', (req, res) => {
   console.log('Alguien se conectó y solicitó info');
